4d41bf7e997be7ca1832de8a661e972fdd239e37
Mr-BlackHat <bl4cksh4d0w7@gmail.com>
2020-03-29 14:58:34 +0530

    Add files via upload

--- update.py
+++ update.py
@@ -12,15 +12,13 @@ def run_command(command):
     else:
         return output
 
-run_command("apt-ftparchive packages ./debfiles/ > ./Packagess")
+run_command("apt-ftparchive packages ./debfiles/ > ./Packagess;")
 
 with open("Packagess") as fp:
 	with open("Packages","w") as fw:
 		line = fp.readline()
-		cnt = 1
+		cnt = 0
 		while line:
-			line = fp.readline()
-
 			if line.startswith("Filename"):
 				dspsn = url + line[21:-5]
 			if line.startswith("Maintainer"):
@@ -29,8 +27,10 @@ with open("Packagess") as fp:
 				line = "Author: MrBlackHat <meblackhat@protonmail.com>\nDepiction: {}\n".format(dspsn)
 			fw.write(line)
 			# print(line)
+			line = fp.readline()
 			cnt += 1
 
 run_command("bzip2 -c9k ./Packages > ./Packages.bz2")
 run_command("""printf "Origin: MrBlackHat's Repo\nLabel: MrBlackHat\nSuite: stable\nVersion: 1.0\nCodename: MrBlackHat\nArchitecture: iphoneos-arm\nComponents: main\nDescription: MrBlackHat's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release""")
-run_command("""ls ./debfiles/ -t | grep '.deb' | perl -e 'use JSON; @in=grep(s/\n$//, <>); $count=0; foreach $fileNow (@in) { $fileNow = "./debfiles/$fileNow"; $size = -s $fileNow; $debInfo = `dpkg -f $fileNow`; $section = `echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`; $name= `echo "$debInfo" | grep "Name: " | cut -c 7- | tr -d "\n\r"`; $version= `echo "$debInfo" | grep "Version: " | cut -c 10- | tr -d "\n\r"`; $package= `echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`; $time= `date -r $fileNow +%s | tr -d "\n\r"`; @in[$count] = {section=>$section, package=>$package, version=>$version, size=>$size+0, time=>$time+0, name=>$name}; $count++; } print encode_json(\@in)."\n";' > all.packages;""")
+run_command("""ls ./debfiles/ -t | grep '.deb' | perl -e 'use JSON; @in=grep(s/\n$//, <>); $count=0; foreach $fileNow (@in) { $fileNow = "./debfiles/$fileNow"; $size = -s $fileNow; $debInfo = `dpkg -f $fileNow`; $section = `echo "$debInfo" | grep "Section: " | cut -c 10- | tr -d "\n\r"`; $name= `echo "$debInfo" | grep "Name: " | cut -c 7- | tr -d "\n\r"`; $version= `echo "$debInfo" | grep "Version: " | cut -c 10- | tr -d "\n\r"`; $package= `echo "$debInfo" | grep "Package: " | cut -c 10- | tr -d "\n\r"`; $time= `date -r $fileNow +%s | tr -d "\n\r"`; @in[$count] = {section=>$section, package=>$package, version=>$version, size=>$size+0, time=>$time+0, name=>$name}; $count++; } print encode_json(\@in)."\n";' > all.packages""")
+# if you got error of empty all.packages file try to sudo cpan JSON to resolve JSON.pm error 
